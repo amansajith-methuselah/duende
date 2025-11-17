b4be70393a57897b97e79bec837667521fa30168
amansajith-methuselah <aman@methuselahventures.ai>
2025-11-17 10:41:43 +0530

    added ignore file

--- BffServer/obj/Debug/net8.0/BffServer.AssemblyInfo.cs
+++ BffServer/obj/Debug/net8.0/BffServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BffServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf3d3508e2dccf52d97e691e68afaed091f4e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bdca0e4de70fb50fe32bf3f50d27a1c47559eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BffServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BffServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
